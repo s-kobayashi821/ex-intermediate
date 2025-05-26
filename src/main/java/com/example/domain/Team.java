38d9832f1decdbacdec451d44b1ed5b4c69ae196
S.Kobayashi <shunsuke.kobayashi@rakus.co.jp>
2025-05-26 10:05:32 +0900

    add ex1:domain

---
+++ src/main/java/com/example/domain/Team.java
@@ -0,0 +1,9 @@
+package com.example.domain;
+
+public class Team {
+    private  Integer id;
+    private String leagueName;
+    private  String headquarters;
+    private String inauguration;
+    private String history;
+}
